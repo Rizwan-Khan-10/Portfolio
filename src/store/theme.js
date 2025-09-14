1c22ae4a99627c1ff6465df0ed78f7b82109e2f1
Rizwan-Khan-10 <rizwankn49@gmail.com>
2025-09-14 17:30:35 +0530

    Added vercel.json

--- src/store/theme.js
+++ src/store/theme.js
@@ -4,7 +4,7 @@ import { persist } from "zustand/middleware";
 const useThemeStore = create(
     persist(
         (set, get) => ({
-            isDark: false,
+            isDark: true,
             toggleTheme: () => set({ isDark: !get().isDark }),
         }),
         {
